33198b3dbebbc4522e5dd9769d1ef192f37bbcc7
roycechua23 <royce236@gmail.com>
2020-07-16 00:58:48 +0800

    Add delete dispatch function

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,6 +3,8 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
     switch (action.type) {
+        case "delete_blogpost":
+            return state.filter((blogPost) => blogPost.id !== action.payload);
         case "add_blogpost":
             return [...state, { id: Math.floor(Math.random() * 99999), title: `Blog Post#${state.length + 1}` }];
         default:
@@ -15,6 +17,12 @@ const addBlogPost = (dispatch) => {
         dispatch({ type: 'add_blogpost' });
     }
 };
+
+const deleteBlogPost = (dispatch) => {
+    return (id) => {
+        dispatch({ type: 'delete_blogpost', payload: id })
+    }
+}
 
 export const { Context, Provider } = createDataContext(
     blogReducer, 
