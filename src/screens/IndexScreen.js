660889bd7f71e2f6ca35a86cc9b01b7c22ed9e03
roycechua23 <royce236@gmail.com>
2020-07-15 23:31:51 +0800

    Add UseContext code to IndexScreen

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import { View, Text, Style, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, Style, StyleSheet, FlatList } from 'react-native';
+import BlogContext, { BlogProvider } from '../context/BlogContext';
 
 const IndexScreen = () => {
+    const blogPosts = useContext(BlogContext);
+
     return (
         <View>
             <Text>Index Screen</Text>
+            <FlatList
+                data={blogPosts}
+                keyExtractor={(blogPost) => blogPost.title}
+                renderItem={({ item }) => {
+                    return <Text>{item.title}</Text>
+                }}
+            />
         </View>
     );
 };
