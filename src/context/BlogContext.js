e8209b9e6ae4e48f38ae6f59a9415d990c06c376
roycechua23 <royce236@gmail.com>
2020-07-17 01:23:38 +0800

    Add edit_blogpost action type

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,49 +1,52 @@
-import React, { useReducer } from 'react';
-import createDataContext from './createDataContext';
+import React, { useReducer } from "react";
+import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
-    switch (action.type) {
-        case "delete_blogpost":
-            return state.filter((blogPost) => blogPost.id !== action.payload);
-        case "add_blogpost":
-            return [
-                ...state, 
-                { 
-                    id: Math.floor(Math.random() * 99999), 
-                    title: action.payload.title,
-                    content: action.payload.content, 
-                }
-            ];
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "delete_blogpost":
+      return state.filter((blogPost) => blogPost.id !== action.payload);
+    case "add_blogpost":
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: action.payload.title,
+          content: action.payload.content,
+        },
+      ];
+    case "edit_blogpost":
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
+    default:
+      return state;
+  }
 };
 
 const addBlogPost = (dispatch) => {
-    return (title, content, callback) => {
-        dispatch({ type: 'add_blogpost', payload: { title, content } });
-        callback();
-    }
+  return (title, content, callback) => {
+    dispatch({ type: "add_blogpost", payload: { title, content } });
+    callback();
+  };
 };
 
 const deleteBlogPost = (dispatch) => {
-    return (id) => {
-        dispatch({ type: 'delete_blogpost', payload: id })
-    }
-}
+  return (id) => {
+    dispatch({ type: "delete_blogpost", payload: id });
+  };
+};
 
 const editBlogPost = (dispatch) => {
-    return (id, title, content) => {
-        dispatch({ 
-            type: 'edit_blogpost', 
-            payload: { id, title, content }
-        });
-    }
-}
+  return (id, title, content) => {
+    dispatch({
+      type: "edit_blogpost",
+      payload: { id, title, content },
+    });
+  };
+};
 
 export const { Context, Provider } = createDataContext(
-    blogReducer, 
-    { addBlogPost, deleteBlogPost, editBlogPost },
-    [{ title: 'TEST POST', content: 'TEST CONTENT', id: 1 }],
+  blogReducer,
+  { addBlogPost, deleteBlogPost, editBlogPost },
+  [{ title: "TEST POST", content: "TEST CONTENT", id: 1 }]
 );
-
