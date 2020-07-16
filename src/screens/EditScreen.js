5be63f8d4b9f5bcca7564c02d9129ab3795d9d33
roycechua23 <royce236@gmail.com>
2020-07-16 22:10:48 +0800

    Add EditScreen.js

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { Context } from '../context/BlogContext';
 
-const EditScreen = () => {
+const EditScreen = ({ navigation }) => {
+    const { state } = useContext(Context);
+
+    const blogPost = state.find(
+        (blogPost) => blogPost.id === navigation.getParam('id')
+    );
+
+    const [title, setTitle] = useState(blogPost.title);
+    const [content, setContent] = useState(blogPost.content);
+
     return <View>
-        <Text>Edit Screen</Text>
+        <Text>Edit Title</Text>
+        <TextInput value={title} onChangeText={(newTitle) => setTitle(newTitle)} />
+        <TextInput value={content} onChangeText={(newContent) => setContent(newContent)} />
     </View>
 };
 
