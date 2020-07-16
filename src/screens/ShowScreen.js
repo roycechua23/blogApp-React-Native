54725d68576bb9aa917414f79651cb1cfc6cbe12
roycechua23 <royce236@gmail.com>
2020-07-16 21:52:25 +0800

    Add headerRight edit icon for EditScreen

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Context } from '../context/BlogContext';
+import { EvilIcons } from '@expo/vector-icons';
 
 const ShowScreen = ({ navigation }) => {
     const { state } = useContext(Context);
@@ -13,6 +14,16 @@ const ShowScreen = ({ navigation }) => {
         <Text>{blogPost.content}</Text>
       </View>
     );
+};
+
+ShowScreen.navigationOptions = () => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate('Edit')}>
+        <EvilIcons name="pencil" size={35} />
+      </TouchableOpacity>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({});
