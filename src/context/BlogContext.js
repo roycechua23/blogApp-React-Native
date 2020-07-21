5ba00a0787ae7530b05364416795327d9df25b94
roycechua23 <royce236@gmail.com>
2020-07-21 20:11:41 +0800

    add get_blogposts action

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,8 +1,11 @@
 import React, { useReducer } from "react";
 import createDataContext from "./createDataContext";
+import jsonServer from "../api/jsonServer";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case "get_blogposts":
+      return action.payload;
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
     case "add_blogpost":
@@ -23,11 +26,20 @@ const blogReducer = (state, action) => {
   }
 };
 
+const getBlogPosts = (dispatch) => {
+  return async () => {
+    const response = await jsonServer.get("/blogposts");
+    // response.data === [{}, {}, {}]
+    dispatch({ type: 'get_blogposts', payload: response.data });
+  };
+};
+
 const addBlogPost = (dispatch) => {
-  return (title, content, callback) => {
-    dispatch({ type: "add_blogpost", payload: { title, content } });
+  return async (title, content, callback) => {
+    await jsonServer.post('/blogposts', { title, content })
+    // dispatch({ type: "add_blogpost", payload: { title, content } });
     if (callback) {
-        callback();
+      callback();
     }
   };
 };
@@ -45,13 +57,13 @@ const editBlogPost = (dispatch) => {
       payload: { id, title, content },
     });
     if (callback) {
-        callback();
+      callback();
     }
   };
 };
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost, editBlogPost },
-  [{ title: "TEST POST", content: "TEST CONTENT", id: 1 }]
+  { addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts },
+  []
 );
